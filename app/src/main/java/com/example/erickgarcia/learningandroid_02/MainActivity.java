72f05b57c8f256fecc5a43c6307e6a6ce90e8863
ImErick <3r1ck.442@gmail.com>
2017-07-02 23:40:37 -0500

    implement a basic listView example

--- app/src/main/java/com/example/erickgarcia/learningandroid_02/MainActivity.java
+++ app/src/main/java/com/example/erickgarcia/learningandroid_02/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.erickgarcia.learningandroid_02;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listView);
+        // datos para a mostrar en el listView, es scrolleable
+        List<String> names  = new ArrayList<>();
+        names.add("erick");
+        names.add("monse");
+        names.add("abril");
+
+        // Adaptador, la forma visual en que mostrare mis datos
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, names);
+        listView.setAdapter(adapter);
     }
 }
