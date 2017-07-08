1aa452fb14cae8b4352bc59633f00436d266e202
ImErick <3r1ck.442@gmail.com>
2017-07-07 19:27:49 -0500

    implement context menu to delete item in grid view

--- app/src/main/java/com/example/erickgarcia/learningandroid_02/GridActivity.java
+++ app/src/main/java/com/example/erickgarcia/learningandroid_02/GridActivity.java
@@ -2,6 +2,7 @@ package com.example.erickgarcia.learningandroid_02;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -44,8 +45,12 @@ public class GridActivity extends AppCompatActivity {
         // enlazamos con nuestro adaptador personalizado
         myAdapter = new MyAdapter(this, R.layout.grid_item, names);
         gridView.setAdapter(myAdapter);
+
+        //agregar el contextMenu al grid
+        registerForContextMenu(gridView);
     }
 
+    // inflamos el layout de menu de opciones
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -53,6 +58,7 @@ public class GridActivity extends AppCompatActivity {
         return true;
     }
 
+    // manejamos eventos click en el menu de opciones
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
@@ -65,5 +71,32 @@ public class GridActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    // inflamos el layout del context menu
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+        menu.setHeaderTitle(names.get(info.position));
+        inflater.inflate(R.menu.context_menu, menu);
+    }
+
+    // manejamos eventos click en el context menu
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        switch (item.getItemId()){
+            case R.id.delete_item:
+                // remover nombre por posicion en la que se encuentra en el menu
+                this.names.remove(info.position);
+                // notificamos al adaptador
+                this.myAdapter.notifyDataSetChanged();
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
+
     }
 }
